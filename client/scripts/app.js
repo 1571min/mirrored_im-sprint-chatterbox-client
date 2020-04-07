774eca11822a060a6164c716d25d3955c60e05ed
kthdd1234 <kthdd12@daum.net>
2020-04-07 13:02:17 +0900

    change file3

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,60 +1,66 @@
 // eslint-disable-next-line
 const app = {
-  server: 'http://52.78.206.149:3000/messages',
-  init: function () {
-    return;
+  server: "http://52.78.206.149:3000/messages",
+  init: function() {
+    app.fetch().then(res => {
+      for(let i = 0; i < res.length; i++) {
+        app.renderMessage(res[i]);
+      }
+    })
   },
   fetch: function () {
-    fetch(app.server)
-      .then((res) => res.json())
-      .then((res) => {
-        return res;
-      });
+    return fetch(app.server).then(res => res.json())
+    .then(res => {
+      return res;
+    })
   },
-  send: function (message) {
-    window
-      .fetch(app.server, {
-        method: 'POST',
-        body: JSON.stringify(message),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-      .then((response) => {
-        return response.json();
-      })
-      .then((json) => {
-        return json;
-      });
+  send: function(message) {
+    window.fetch(app.server, {
+      method: 'POST',
+      body: JSON.stringify(message),
+      headers: {
+        "Content-Type": "application/json",
+      }
+    }).then(response => {
+      return response.json();
+    }).then(json => {
+      console.log(json)
+      return json;
+    });
   },
-  clearMessages: function () {
-    document.querySelector('#chats').innerHTML = '';
+  clearMessages: function() {
+    document.querySelector("#chats").innerHTML = "";
   },
-  renderMessage: function (message) {
+  renderMessage: function(message) {
     let newElement = document.createElement('div');
     let newUsername = document.createElement('div');
     let newText = document.createElement('div');
-    newElement.className = 'chat';
+    newElement.className = 'chat'
     newUsername.className = 'username';
     newText.className = 'userText';
     newUsername.textContent = message.username + ':';
     newText.textContent = message.text;
     newElement.appendChild(newUsername);
     newElement.appendChild(newText);
-    document.querySelector('#chats').appendChild(newElement);
-  },
+    document.querySelector("#chats").prepend(newElement);
+  }
 };
 
 let summitButton = document.querySelector('#summitButton');
 summitButton.addEventListener('click', function () {
-  let textarea = document.querySelector('#userInput');
-  console.log(textarea);
-  console.log(textarea.value);
-  const message = {
-    username: '김코딩',
-    text: textarea.value,
-    roomname: '로비',
-  };
-
-  app.renderMessage(message);
+let textarea = document.querySelector('#userInput');
+console.log(textarea);
+console.log(textarea.value);
+const message = {
+username: '김코딩',
+text: textarea.value,
+roomname: '로비',
+};
+app.renderMessage(message);
+app.send(message);
+app.fetch();
 });
+
+
+app.init()
+
